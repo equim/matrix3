7b140a002d6fbe4b85df1a969c2504a83fdf52a8
Tavis Ormandy <taviso@gmail.com>
2026-05-18 07:44:12 -0700

    fix bug handling unset options

--- include/options.js
+++ include/options.js
@@ -1,3 +1,12 @@
+
+// The default options if none are set.
+const kDefaultOptions = {
+    defaultpolicy: 0,
+    advanced: false,
+    badges: false,
+    defaultscope: "domain",
+};
+
 // A cache of chrome.storage.sync's `options` key, we want to check options in
 // a lot of hot paths, and a storage.sync.get call everytime is suboptimal.
 export default class Options {
@@ -7,20 +16,15 @@ export default class Options {
 
     static #load() {
         Options.#promise ??= chrome.storage.sync.get("options").then(({ options }) => {
-            // Default configuration for fresh installations.
-            Options.#cache = options || {
-                defaultpolicy: 0,
-                advanced: false,
-                badges: false,
-                defaultscope: "domain",
-            };
+            // Default configuration for fresh installations or partial objects.
+            Options.#cache = Object.assign({}, kDefaultOptions, options);
             chrome.storage.onChanged.addListener((changes) => {
                 if (!changes.options)
                     return;
                 // Mutate in place so consumers can hold a stable reference.
                 for (let key of Object.keys(Options.#cache))
                     delete Options.#cache[key];
-                Object.assign(Options.#cache, changes.options.newValue ?? {});
+                Object.assign(Options.#cache, kDefaultOptions, changes.options.newValue);
                 for (let callback of Options.#listeners)
                     callback(Options.#cache);
             });
